74daf063c41ce0fcb4a67fbad39a21d29ccc20a7
Daniela Lopez <13001824@alumnos.utleon.edu.mx>
2024-09-13 19:02:46 -0600

    comentario basesAngular

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'basesAngular';
+
+pelicula={
+  nombre: 'Super Man',
+  fechaLanzamiento:new Date(), //para poner la fecha actual
+  precio: 123.65
+}
+  duplicarNumero(valor:number):number{
+    return valor*2;
+  }
+}
