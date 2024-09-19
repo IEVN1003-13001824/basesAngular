5368865b9b063bd1b055a449672c7515ed43e0cb
Daniela Lopez <13001824@alumnos.utleon.edu.mx>
2024-09-18 19:04:47 -0600

    18 de septiembre del 2024-Operaciones Basicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
+import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductoFilterPipe
+    ProductoFilterPipe,
+    OperasBasComponent
   ],
   imports: [
     BrowserModule,
